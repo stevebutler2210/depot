b6750e41f44457c3295112c21e7c1bcac1ab1a60
Steve Butler <steve.butler@kyan.com>
2017-08-14 13:42:20 +0100

    More work on Orders / Order form

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,7 @@ class Order < ApplicationRecord
     'Credit Card' => 1,
     'Purchase Order' => 3
   }
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
 end
